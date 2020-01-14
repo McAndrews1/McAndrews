14ce0af51784398dd5a2b38fa645932957c053f0
McAndrews <lgmcandrews@gmail.com>
2020-01-14 17:21:10 +0100

    Un comentario saliente por consola

---
+++ PosNada.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class PosNada here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class PosNada
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public static void sampleMethod()
+    {
+        // put your code here
+        System.out.println("Tonto el que lo lea. XD");
+    }
+}
