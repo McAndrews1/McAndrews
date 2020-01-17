795403106fe8e0179e629e3c829c0f37f076a671
McAndrews <lgmcandrews@gmail.com>
2020-01-17 18:35:56 +0100

    j

--- voxxxxx.java
+++ voxxxxx.java
@@ -1,5 +1,6 @@
 
 /**
+<<<<<<< HEAD
  * Write a description of class voxxxxx here.
  * 
  * @author (Boki) 
@@ -21,6 +22,29 @@ public class voxxxxx
 
     /**
      * An example of a method - replace this comment with your own
+=======
+ * Esta clase es completamente inutil,pero si miras, pagas.
+ * 
+ * @author (Boki) 
+ * @version (versión 10€ por mirar)
+ */
+public class voxxxxx
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Es el construcctor de la clase
+     */
+    public voxxxxx()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * Este método no hace nada y es completamente inutil
+>>>>>>> 1d6eb1b49d6c66d448ac4c5abb658c459c06c483
      * 
      * @param  y   introduce un valor int que no sirve para nada
      * @return     devuelve x + y, aunque sin estar inicializada será inutil.
