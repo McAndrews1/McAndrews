74b470c55ea30572fb0b861104e7c8b02952a63c
Borja <borjagarciabarquin@gmail.com>
2020-01-17 18:38:40 +0100

    uu

--- voxxxxx.java
+++ voxxxxx.java
@@ -1,61 +1,9 @@
-
 /**
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
- * Write a description of class voxxxxx here.
- * 
- * @author (Boki) 
- * @version (versión 10€ por mirar)
- */
-public class voxxxxx
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Es el construcctor de la clase
-     */
-    public voxxxxx()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-=======
- * Esta clase es completamente inutil,pero si miras, pagas.
- * 
- * @author (Boki) 
- * @version (versión 10€ por mirar)
- */
-public class voxxxxx
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Es el construcctor de la clase
-     */
-    public voxxxxx()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * Este método no hace nada y es completamente inutil
->>>>>>> 1d6eb1b49d6c66d448ac4c5abb658c459c06c483
-     * 
-     * @param  y   introduce un valor int que no sirve para nada
-=======
- * Esta clase es completamente inutil,pero si miras, pagas.
- * 
- * @author (Boki) 
- * @version (versión 10€ por mirar http://paypal.me/bokigod)
- * @see https://paypal.me/bokigod
- */
+* Esta clase es completamente inutil,pero si miras, pagas.
+* 
+* @author (Boki) 
+* @version (versión 10€ por mirar http://paypal.me/bokigod)
+*/
 public class voxxxxx
 {
     // instance variables - replace the example below with your own
@@ -75,33 +23,7 @@ public class voxxxxx
      * 
      * @param  y   introduce un valor int que no sirve para nada
      * @see MeDebes10Euros
->>>>>>> e2d0a10c3d196fc6ed55ef92b7d52ed34fe59e83
-=======
- * Esta clase es completamente inutil,pero si miras, pagas.
- * 
- * @author (Boki) 
- * @version (versión 10€ por mirar http://paypal.me/bokigod)
- */
-public class voxxxxx
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Es el construcctor de la clase
-     */
-    public voxxxxx()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * Este método no hace nada y es completamente inutil
-     * 
-     * @param  y   introduce un valor int que no sirve para nada
-     * @see MeDebes10Euros
->>>>>>> e2d0a10c3d196fc6ed55ef92b7d52ed34fe59e83
+    >>>>>>> e2d0a10c3d196fc6ed55ef92b7d52ed34fe59e83
      * @return     devuelve x + y, aunque sin estar inicializada será inutil.
      */
     public int sampleMethod(int y)
