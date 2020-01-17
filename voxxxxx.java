38f1fe258cdbea992efc2205387e7680aeca14f3
Borja <borjagarciabarquin@gmail.com>
2020-01-17 18:40:15 +0100

    fuk

--- voxxxxx.java
+++ voxxxxx.java
@@ -3,7 +3,6 @@
 * 
 * @author (Boki) 
 * @version (versión 10€ por mirar http://paypal.me/bokigod)
-* @see https://paypal.me/bokigod
 */
 public class voxxxxx
 {
@@ -24,7 +23,6 @@ public class voxxxxx
      * 
      * @param  y   introduce un valor int que no sirve para nada
      * @see MeDebes10Euros
-    >>>>>>> e2d0a10c3d196fc6ed55ef92b7d52ed34fe59e83
      * @return     devuelve x + y, aunque sin estar inicializada será inutil.
      */
     public int sampleMethod(int y)
